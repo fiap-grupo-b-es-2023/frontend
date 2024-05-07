989c65a61cc35df8133e2c96bc62331f0970929e
lucasfonseca19 <lucasofa808@gmail.com>
2024-05-07 17:49:35 -0300

    Add ProductDetailsPage with modal layout and product details
    - Implement `ProductDetailsPage` to show detailed product info in a modal.
    - Create increment and decrement buttons to adjust product quantity.
    - Provide an "Add to Cart" button to include the selected product in the cart.
    - Style the modal using `ProductDetailsPage.module.css` for a clean design.
    - Adjust modal footer with options to cancel or add the product to the cart.

---
+++ src/pages/ProductDetailsPage/ProductDetailsPage.jsx
@@ -0,0 +1,92 @@
+import {useContext, useState} from 'react';
+import {Modal, Button, Row, Col, Image} from 'react-bootstrap';
+import {CartContext} from "../../CartContext.jsx";
+import style from './ProductDetailsPage.module.css';
+
+export default function ProductDetailsPage({product, closeModal}) {
+    const [quantity, setQuantity] = useState(1); // Supondo que você queira gerenciar a quantidade
+
+    // Funções para incrementar e decrementar a quantidade
+    const incrementQuantity = () => setQuantity(prevQuantity => prevQuantity + 1);
+    const decrementQuantity = () => setQuantity(prevQuantity => (prevQuantity > 1 ? prevQuantity - 1 : 1));
+
+    const {addToCart } = useContext(CartContext);
+    const handleAddToCart = () => {
+        addToCart(product, quantity);
+        closeModal();
+      };
+    
+    return (
+        <Modal
+            show={true}
+            onHide={closeModal}
+            centered
+            contentClassName={style.modalContent}
+            size="lg"
+        >
+            <Modal.Body>
+                <Row className="justify-content-end">
+                    <Col md={4}>
+                        <Image src="https://via.placeholder.com/200" thumbnail fluid/>
+                    </Col>
+                    <Col md={8}>
+                        <Row>
+                            <Col md={6}>
+                                <p className="fw-bold mb-2 ">Nome do Produto</p>
+                                <p>{product.name}</p>
+                            </Col>
+                            <Col md={6}>
+                                <p className="fw-bold mb-2">Marca</p>
+                                <p>{product.brand}</p>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col md={6}>
+                                <p className="fw-bold mb-2">Linha</p>
+                                <p>{product.line}</p>
+                            </Col>
+                            <Col md={6}>
+                                <p className="fw-bold mb-2">Família</p>
+                                <p>{product.family}</p>
+                            </Col>
+
+                        </Row>
+                        <Row>
+                            <Col md={12}>
+                                <p className="fw-bold mb-2">Código</p>
+                                <p>{product.code}</p>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+                <Row className="justify-content-center mt-3">
+                    <Col md={4} className="text-center">
+                        <p>R$ {(product.price * quantity).toFixed(2)}</p>
+                        <Row className="justify-content-center align-items-center">
+                            <Col xs="auto">
+                                <Button variant="outline-secondary" onClick={decrementQuantity}>-</Button>
+                            </Col>
+                            <Col xs="auto" className="d-flex justify-content-center align-items-center">
+                                <span className={style.quantity}>{quantity}</span>
+                            </Col>
+                            <Col xs="auto">
+                                <Button className={style.addButton} onClick={incrementQuantity}>+</Button>
+                            </Col>
+                        </Row>
+
+                    </Col>
+                </Row>
+            </Modal.Body>
+            <Modal.Footer className={`${style.modalFooter} d-flex justify-content-around gap-2`}>
+                <Button variant="link text-white" onClick={() => closeModal()}>
+                    Cancelar
+                </Button>
+                <Button className={style.addButton} onClick={handleAddToCart} >
+              
+                    Adicionar
+                </Button>
+            </Modal.Footer>
+        </Modal>
+
+    );
+}
